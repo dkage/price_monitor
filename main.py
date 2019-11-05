8b941610b95010cf66686f0ba5d3f6225faf5e07
Danilo Cunha <danilokage@gmail.com>
2019-11-05 19:48:50 -0300

    Finish Kabum request GET structure for both used URLs

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ from trackers.kabum_tracker import *
 
 cpu = 102438  # Ryzen 5 3600
 print(get_kabum(cpu))
+
+
+taichi = 102175  # Ryzen 5 3600
+print(get_kabum(taichi))
+cpu_2 = 102436  # Ryzen 5 3600
+print(get_kabum(cpu_2))
--- trackers/kabum_tracker.py
+++ trackers/kabum_tracker.py
@@ -8,12 +8,28 @@ def get_kabum (product_code):
     http_return = requests.get(base_url + str(product_code))
     soup = BeautifulSoup(http_return.content, 'lxml')
 
+    # Initialize
     product = dict()
+    url_redirect = str()
 
-    # TODO create case where product has promotion
-    product["product_name"] = soup.find('h1', {"class": "titulo_det"}).text
-    print(product["product_name"])
-    product["price"] = soup.find('div', {"class": "preco_desconto-cm"}).find('strong').text
-    product["price_cash"] = soup.find('span', {"class": "preco_desconto_avista-cm"}).text
+    # Kabum uses two different URL structures. If we use 'base_url' variable and put code of a product that uses the
+    # other type, page requested it is a simple meta redirect. So we grab that url to receive the true URL.
+    if len(str(soup)) < 1000:  # If soup is too small, it's obviously a redirect page, so we grab url and GET again.
+        for tag in soup.find_all("meta"):
+            if 'url=' in tag.get('content'):
+                url_redirect = str(tag.get('content')).split('=', 1)[1]
+
+        http_return = requests.get(url_redirect)
+        soup = BeautifulSoup(http_return.content, 'lxml')
+
+    # If product is on sale, different HTML elements are used. If on sale contains div class "contTEXTO" else it's not.
+    if soup.find('div', {'class': 'contTEXTO'}):
+        product["product_name"] = soup.find('h1', {"class": "titulo_det"}).text
+        product["price"] = soup.find('div', {"class": "preco_desconto-cm"}).find('strong').text
+        product["price_cash"] = soup.find('span', {"class": "preco_desconto_avista-cm"}).text
+    else:
+        product["product_name"] = soup.find('h1', {"class": "titulo_det"}).text
+        product["price"] = str(soup.find('div', {"class": "preco_normal"}).text).strip()
+        product["price_cash"] = soup.find('span', {"class": "preco_desconto"}).find('strong').text
 
     return product
