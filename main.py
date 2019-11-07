9bf1031a6a1f5c90a3a8540c2ced9bd16e511a8a
Danilo Cunha <danilokage@gmail.com>
2019-11-07 19:37:34 -0300

    Add more tests to main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from trackers.kabum_tracker import *
+from trackers.kabum_functions import *
+from trackers.pichau_functions import *
 
-
-cpu = 102438  # Ryzen 5 3600
-print(get_kabum(cpu))
+cpu = dict()
+cpu['kabum'] = 102438  # Ryzen 5 3600
+cpu['pichau'] = 'processador-amd-ryzen-5-3600-hexa-core-3-6ghz-4-2ghz-turbo-35mb-cache-am4-yd3600bbafbox'  # Ryzen 5 3600
+# print(get_kabum(cpu['kabum']))
+print(get_pichau(cpu['pichau']))
 
 
 taichi = 102175  # Ryzen 5 3600
-print(get_kabum(taichi))
+# print(get_kabum(taichi))
 cpu_2 = 102436  # Ryzen 5 3600
-print(get_kabum(cpu_2))
+# print(get_kabum(cpu_2))
