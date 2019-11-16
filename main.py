c3a6e8641ec9aa5bb896660b84222b51c433077b
Danilo Cunha <danilokage@gmail.com>
2019-11-15 23:27:29 -0300

    Add basic html template for home page

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return get_terabyte(psu['terabyte'])
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1500)
+    app.run(debug=True)
