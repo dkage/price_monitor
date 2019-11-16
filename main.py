48600dba7f5a1141862fb4eb405527acad5dd3d4
Danilo Cunha <danilokage@gmail.com>
2019-11-16 01:50:55 -0300

    Add import and function call on main

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
+from functions import *
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', products_dict)
 
 
 if __name__ == "__main__":
+    products_dict = generate_prices_dict()
     app.run(debug=True)
