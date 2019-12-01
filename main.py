b118f1e509ad529ad919c144070b8dd99c98d0cf
Danilo Cunha <danilokage@gmail.com>
2019-12-01 00:05:56 -0300

    Fix indentation for Jinja2

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
+from config.config import Config
 from functions import *
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    # products_dict = generate_prices_dict()
-    products = get_products_array()
-    print(products)
+    for config in app.config:
+        print(config)
+        print(app.config[config])
+    # products = get_products_array()
+    # print(products)
 
     # app.run(debug=True)
