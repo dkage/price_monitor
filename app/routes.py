1fedf4f4b548ef37331e090448e57195f958c2bb
Danilo Cunha <danilokage@gmail.com>
2019-12-03 23:32:59 -0300

    Experimenting with forms redirect and store submit data

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.flask_app import *
 from app.forms import *
 
@@ -11,7 +11,12 @@ def index():
     return render_template("index.html", user=user)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
     return render_template('login.html', title="Login test page", form=form)
