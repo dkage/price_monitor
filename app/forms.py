230aaa6a6a4ab695738761911d0989567b7fa9ff
Danilo Cunha <danilokage@gmail.com>
2019-12-07 14:37:07 -0300

    Add new form class for add new product

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,7 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class NewProduct(FlaskForm):
+    product_name = StringField('ProductName', validators=[DataRequired()])
