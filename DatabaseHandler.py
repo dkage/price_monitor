0f4a96b466673680dfa37672e4ed724b3158faa2
Danilo Cunha <danilokage@gmail.com>
2019-12-13 19:20:57 -0300

    Add new method to add product to database, and set autocommit

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -15,6 +15,7 @@ class DatabaseHandler:
                                       port=self.db_info.get('database_connection', 'port'),
                                       user=self.db_info.get('database_connection', 'username'),
                                       password=self.db_info.get('database_connection', 'password'))
+        connection.autocommit = True
         connected_cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
         self.cursor = connected_cursor
 
@@ -25,7 +26,26 @@ class DatabaseHandler:
 
         return fetched_array
 
-    def insert_new_product(self):
+    def add_new_product(self, form_data):
+
+        # TODO add function to trim links string of not needed parts of URL
+        self.cursor.execute("INSERT INTO products ("
+                            "product_type, "
+                            "product_name, "
+                            "product_description,"
+                            "link_kabum, "
+                            "link_pichau, "
+                            "link_terabyte"
+                            ") VALUES (%s, %s, %s, %s, %s, %s)",
+                            (
+                                form_data['product_type'],
+                                form_data['product_name'],
+                                form_data['product_desc'],
+                                form_data['kabum_link'],
+                                form_data['pichau_link'],
+                                form_data['terabyte_link']
+                            ))
+
         return 'ok'
 
     def add_new_best_price(self):
