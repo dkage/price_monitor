6478f3d54da6b003409ef8509f2b0b8489d5b961
Danilo Cunha <danilokage@gmail.com>
2020-01-13 00:43:46 -0300

    Start of logic for grabbing prices of database registered products

--- test.py
+++ test.py
@@ -1,20 +1,36 @@
+from DatabaseHandler import DatabaseHandler
+from static.static_vars import *
 from functions import *
 import configparser
 import psycopg2.extras
 import re
 import json
-from DatabaseHandler import DatabaseHandler
+
+
+def link_valid(link):
+    if link == 'Invalid link given.':
+        return False
+    else:
+        return True
 
 
 db_handler = DatabaseHandler()
-products = db_handler.get_products_array()
 
 
-for product in products:
-    kabum = get_kabum(product['link_kabum'])
-    pichau = get_pichau(product['link_pichau'])
-    terabyte = get_terabyte(product['link_terabyte'])
-    print(kabum)
-    print(pichau)
-    print(terabyte)
+product_array = db_handler.select_all_products()
+for product in product_array:
+    print(product)
+    print('Looking for product ID: {}'.format(product[0]))
+    print('Name: {}'.format(product[2]))
+
+    kabum_price = get_kabum(product[4]) if link_valid(product[4]) else 0
+    pichau_price = get_pichau(product[5]) if link_valid(product[4]) else 0
+    terabyte_price = get_terabyte(product[6]) if link_valid(product[4]) else 0
+
+    product[4] = kabum_price
+    product[5] = pichau_price
+    product[6] = terabyte_price
+
+print(product_array)
+
 
