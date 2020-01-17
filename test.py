589e921f0663b4694b772a549f7442a0850bdb0c
Danilo Cunha <danilokage@gmail.com>
2020-01-16 23:55:38 -0300

    Create list of prices to add to bank after scrapping

--- test.py
+++ test.py
@@ -18,7 +18,9 @@ db_handler = DatabaseHandler()
 
 
 product_array = db_handler.select_all_products()
+prices_array = []
 for product in product_array:
+    product_scrap = dict()
     print(product)
     print('Looking for product ID: {}'.format(product[0]))
     print('Name: {}'.format(product[2]))
@@ -27,10 +29,9 @@ for product in product_array:
     pichau_price = get_pichau(product[5]) if link_valid(product[4]) else 0
     terabyte_price = get_terabyte(product[6]) if link_valid(product[4]) else 0
 
-    product[4] = kabum_price
-    product[5] = pichau_price
-    product[6] = terabyte_price
+    product_scrap['id'] = product[0]
+    product_scrap['kabum_price'] = kabum_price
+    product_scrap['pichau_price'] = pichau_price
+    product_scrap['terabyte_price'] = terabyte_price
 
-print(product_array)
-
-
+    prices_array.append(product_scrap)
