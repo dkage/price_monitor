a82bfa04665d9ef3faaf238cc4e33ab0a3185e61
Danilo Cunha <danilokage@gmail.com>
2020-01-17 18:56:36 -0300

    Add comment and store array to static_vars.py

--- static/static_vars.py
+++ static/static_vars.py
@@ -1,3 +1,7 @@
+# Use here name of dictionary key to be used during scraper for best prices
+stores_analyzed = ['kabum', 'pichau', 'terabyte']
+
+# Base URL for each store, this should be concatenated with the data stored in DB for full url link
 pichau_base_url = 'https://www.pichau.com.br/'
 kabum_base_url = 'https://www.kabum.com.br/cgi-local/site/produtos/descricao_ofertas.cgi?codigo='
 terabyte_base_url = 'https://www.terabyteshop.com.br/produto/'
