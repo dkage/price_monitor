4d1548e142407ec4092e10050f93085bb0a14a90
Danilo Cunha <danilokage@gmail.com>
2020-02-18 14:33:48 -0300

    Add code to enable flask and scraper to run concurrently

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from app.routes import *
+from time import sleep
+import threading
+
+
+class RunScraperHourly(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.board = 1
+
+    def run(self):
+        while True:
+            sleep(20)
+            print('Starting scraper')
+            get_best_values()
+            print('Scraper finished. Running again in one hour.')
+            sleep(3600)
+
+
+scraper_run = RunScraperHourly()
 
 
 if __name__ == "__main__":
+    scraper_run.start()
     app.run(host='0.0.0.0', debug=True)
